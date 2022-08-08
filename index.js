aeae96b43e235493c90bdb88dcaf2d309bed006b
Diana Sushik <dianasushik2000@gmail.com>
2022-08-07 22:27:09 -0700

    commit tree a50aa1de41fe42badd78f53a4c9b17f70f5280b3

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+const random = Math.floor(Math.random()* 10) + 1;
+
+const num3 = 22;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = (2, 5, 10, 20);
+Math.max();
